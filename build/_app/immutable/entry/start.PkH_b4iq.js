9fdbfc30f109235d7e408487bc8e74b79395315d
Christian6-15 <3108495703@qq.com>
2025-04-07 04:08:19 +0800

    Add build directory for GitHub Pages

---
+++ build/_app/immutable/entry/start.PkH_b4iq.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CZFj2HiF.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node'; // 替换为 adapter-node
+import adapter from '@sveltejs/adapter-static'; // 替换为 adapter-static
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,15 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build', // 静态文件输出目录
+      assets: 'build', // 静态资源输出目录
+      fallback: 'index.html', // 单页应用需要 fallback
+      precompress: false // 是否预压缩文件
+    }),
+    paths: {
+      base: '/svelte_project' // 必须与 GitHub Pages 的路径匹配
+    }
   }
 };
 
